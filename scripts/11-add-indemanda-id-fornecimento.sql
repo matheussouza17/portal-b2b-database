20f5b63cf3143fc185865868cd814e3f9f1c1e10
Matheus Henrique <109185171+matheussouza17@users.noreply.github.com>
2026-06-10 09:07:15 -0300

    Add id_fornecimento and id_solicitacao_frete columns

--- scripts/11-add-indemanda-id-fornecimento.sql
+++ scripts/11-add-indemanda-id-fornecimento.sql
@@ -1,2 +1,3 @@
 -- Adds the id_fornecimento column to the demanda table in portal_b2b schema
 ALTER TABLE portal_b2b.demanda ADD COLUMN id_fornecimento VARCHAR;
+ALTER TABLE portal_b2b.demanda ADD COLUMN id_solicitacao_frete VARCHAR;
